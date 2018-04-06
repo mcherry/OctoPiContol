4197330e834157da300aad5d86b01623c23d2230
Michael Cherry <mcherry@sps186.org>
2018-04-06 14:28:54 -0500

    Breaking things while I fix indentations...

--- src/octopicontrol.py
+++ src/octopicontrol.py
@@ -18,6 +18,7 @@ from pytz import timezone
 from pygame.locals import *
 
 # required environment variables for pygame
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
