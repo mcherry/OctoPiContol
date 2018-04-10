287c0e039939a4e29d67d883dcefae1e16394570
Mike Cherry <mcherry@inditech.org>
2018-04-09 20:53:14 -0500

    adding basic confirmation dialogs

--- src/octopicontrol.py
+++ src/octopicontrol.py
@@ -239,7 +239,7 @@ def confirm(screen, message):
     BLACK = (0,0,0)
     
     Button1 = pygame.Rect(280, 300, 75, 150)
-    Button2 = pygame.Rect(160, 455, 75, 150)
+    Button2 = pygame.Rect(160, 300, 75, 150)
     font = pygame.font.Font(get_script_path() + "/Fonts/NotoMono-Regular.ttf", 13)
     background = createSurface(screen, BLACK)
     
